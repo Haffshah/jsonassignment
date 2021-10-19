4d75eecd73b5b1ac57be42a6400323e9bb9e0744
Harsh M Shah <shahharsh7786@gmail.com>
2021-10-19 19:34:55 +0530

    ListView

--- lib/FollowersPage.dart
+++ lib/FollowersPage.dart
@@ -5,35 +5,50 @@ class FollowersPage extends StatefulWidget {
   final ProfileData? profileData;
   const FollowersPage({Key? key, required this.profileData}) : super(key: key);
 
-
   @override
   _FollowersPageState createState() => _FollowersPageState();
 }
 
 class _FollowersPageState extends State<FollowersPage> {
-  ProfileData? profileData;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Followers"),
+        backgroundColor: Color(0xffD1E8E4),
+        title: Text(
+          "Followers",
+          style: TextStyle(color: Color(0xff191A19)),
+        ),
       ),
       backgroundColor: Color(0xffEADEB8),
-      body: Container(width: double.infinity,
-        height: 150,
-        color: Colors.white,
+      body: Container(
+        width: double.infinity,
         child: ListView.builder(
-          itemCount: profileData?.peopleFollows?.length ?? 0,
+          itemCount: widget.profileData?.peopleFollows?.length ?? 0,
           itemBuilder: (_, int i) {
-            return Center(
-              child: Text(
-                '${profileData?.peopleFollows?[i]}',
-                style: TextStyle(color: Colors.blue),
-              ),
-            );
+            return Container(
+                color: Color(0xff191A19),
+                padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 3),
+                child: ListTile(
+                  leading: Text(
+                    '${i + 1}.',
+                    style: font22,
+                  ),
+                  title: Text(
+                    '${widget.profileData?.peopleFollows?[i]}',
+                    style: font22,
+                  ),
+                ));
           },
         ),
       ),
     );
   }
+
+  TextStyle get font22 => TextStyle(
+      color: Color(0xffD1E8E4),
+      fontFamily: 'Avenir',
+      fontWeight: FontWeight.w500,
+      wordSpacing: 1,
+      fontSize: 22);
 }
--- lib/MyHomePage.dart
+++ lib/MyHomePage.dart
@@ -59,151 +59,170 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ),
       ),
-      body: Column(
-        children: [
-          ClipRRect(
-            borderRadius: const BorderRadius.only(
-              bottomLeft: Radius.circular(12),
-              bottomRight: Radius.circular(12),
-            ),
-            child: Container(
-              child: Image.network(
-                profileData?.image ?? "0",
-                fit: BoxFit.cover,
+      body: Container(
+        height: double.infinity,
+        child: Column(
+          children: [
+            ClipRRect(
+              borderRadius: const BorderRadius.only(
+                bottomLeft: Radius.circular(12),
+                bottomRight: Radius.circular(12),
               ),
-              height: MediaQuery.of(context).size.height / 1.60,
-              width: MediaQuery.of(context).size.width,
-            ),
-          ),
-          SizedBox(
-            height: 10.0,
-          ),
-          Container(
-            child: Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 30.0, vertical: 10),
-              child: Column(
-                children: [
-                  SizedBox(height: 10),
-                  Row(
-                    children: [
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "${profileData?.firstname?.toUpperCase()} ${profileData?.lastname?.toUpperCase()}",
-                            style: TextStyle(
-                                fontFamily: 'Avenir',
-                                fontSize: 15,
-                                fontWeight: FontWeight.w700),
-                          ),
-                          SizedBox(
-                            height: 5,
-                          ),
-                          Text(
-                            ("${profileData?.region ?? " "},${profileData?.country}"),
-                            style: greyFont(),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ],
+              child: Container(
+                child: Image.network(
+                  profileData?.image ?? "0",
+                  fit: BoxFit.cover,
+                ),
+                height: MediaQuery.of(context).size.height / 1.60,
+                width: MediaQuery.of(context).size.width,
               ),
             ),
-          ),
-          SizedBox(
-            height: 19,
-          ),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 30),
-            height: 50,
-            child: Text(
-              ("${profileData?.bio}"),
-              maxLines: 5,
-              style: greyFont(),
+            SizedBox(
+              height: 10.0,
             ),
-          ),
-          Spacer(),
-          Container(
-            alignment: Alignment.bottomCenter,
-            color: Color(0xffFFF8ED),
-            height: 85,
-            width: double.infinity,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
+            Container(
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 30.0, vertical: 10),
+                child: Column(
                   children: [
-                    Text(
-                      'Posts',
-                      style: greyFont(),
-                    ),
-                    SizedBox(
-                      height: 7,
-                    ),
-                    Text(
-                      "${profileData?.post}",
-                      style: TextStyle(
-                        fontSize: 18.0,
-                        fontFamily: 'Avenir',
-                      ),
+                    SizedBox(height: 10),
+                    Row(
+                      children: [
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "${profileData?.firstname?.toUpperCase()} ${profileData?.lastname?.toUpperCase()}",
+                              style: TextStyle(
+                                  fontFamily: 'Avenir',
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w700),
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              ("${profileData?.region ?? " "},${profileData?.country}"),
+                              style: greyFont(),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
                   ],
                 ),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      CupertinoPageRoute(
-                        builder: (context) => FollowersPage(
-                          profileData: profileData,
+              ),
+            ),
+            SizedBox(
+              height: 19,
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 30),
+              height: 50,
+              child: Text(
+                ("${profileData?.bio}"),
+                maxLines: 5,
+                style: greyFont(),
+              ),
+            ),
+            Spacer(),
+            Container(
+              alignment: Alignment.bottomCenter,
+              color: Color(0xffFFF8ED),
+              height: 85,
+              width: double.infinity,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      showModalBottomSheet(
+                          isScrollControlled: true,
+                          backgroundColor: Colors.transparent,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.vertical(
+                                  top: Radius.circular(20))),
+                          context: context,
+                          builder: (context) {
+                            return Bottomsheet(
+                              profileData: profileData,
+                            );
+                          });
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Posts',
+                          style: greyFont(),
                         ),
-                      ),
-                    );
-                  },
-                  child: Column(
+                        SizedBox(
+                          height: 7,
+                        ),
+                        Text(
+                          "${profileData?.post}",
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            fontFamily: 'Avenir',
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        CupertinoPageRoute(
+                          builder: (context) => FollowersPage(
+                            profileData: profileData,
+                          ),
+                        ),
+                      );
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Followers',
+                          style: greyFont(),
+                        ),
+                        SizedBox(
+                          height: 7,
+                        ),
+                        Text(
+                          "${profileData?.followers}",
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            fontFamily: 'Avenir',
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Text(
-                        'Followers',
+                        'Following',
                         style: greyFont(),
                       ),
                       SizedBox(
                         height: 7,
                       ),
                       Text(
-                        "${profileData?.followers}",
-                        style: TextStyle(
-                          fontSize: 18.0,
-                          fontFamily: 'Avenir',
-                        ),
+                        "${profileData?.following}",
+                        style: TextStyle(fontFamily: 'Avenir', fontSize: 18.0),
                       ),
                     ],
                   ),
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Following',
-                      style: greyFont(),
-                    ),
-                    SizedBox(
-                      height: 7,
-                    ),
-                    Text(
-                      "${profileData?.following}",
-                      style: TextStyle(fontFamily: 'Avenir', fontSize: 18.0),
-                    ),
-                  ],
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
@@ -213,4 +232,52 @@ class _MyHomePageState extends State<MyHomePage> {
         color: Colors.grey,
         fontFamily: 'Avenir',
       );
+
+  Widget buildSheet() => Column(
+        children: [],
+      );
+}
+
+class Bottomsheet extends StatelessWidget {
+  const Bottomsheet({
+    Key? key,
+    required this.profileData,
+  }) : super(key: key);
+
+  final ProfileData? profileData;
+
+  @override
+  Widget build(BuildContext context) {
+    return DraggableScrollableSheet(
+      initialChildSize: 0.6,
+      builder: (_, controller) => Container(
+        color: Color(0xff191A19),
+        padding: const EdgeInsets.all(20.0),
+        child: ListView.builder(
+          itemCount: profileData?.peopleFollows?.length ?? 0,
+          itemBuilder: (_, int i) {
+            return Container(
+                color: Color(0xff191A19),
+                child: ListTile(
+                  leading: Text(
+                    '${i + 1}.',
+                    style: font22,
+                  ),
+                  title: Text(
+                    '${profileData?.peopleFollows?[i]}',
+                    style: font22,
+                  ),
+                ));
+          },
+        ),
+      ),
+    );
+  }
+
+  TextStyle get font22 => TextStyle(
+      color: Color(0xffD1E8E4),
+      fontFamily: 'Avenir',
+      fontWeight: FontWeight.w500,
+      wordSpacing: 1,
+      fontSize: 22);
 }
